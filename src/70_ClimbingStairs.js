06115cd03337c078efca88b505533a7b9fa658bb
sig03 <sig03@naver.com>
2022-07-18 12:03:14 +0000

    add logic: not solved

---
+++ src/70_ClimbingStairs.js
@@ -0,0 +1,11 @@
+//https://leetcode.com/problems/climbing-stairs/
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    
+};
+
+const n = 2;
+console.log(climbStairs(n));

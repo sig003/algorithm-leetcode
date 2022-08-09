f71fe1dd615a88c10c9fdf9ae5d39691112924fe
sig03 <sig03@naver.com>
2022-08-09 04:47:34 +0000

    modify code

--- src/programmers_NumberSpelling.js
+++ src/programmers_NumberSpelling.js
@@ -1,23 +1,13 @@
 //https://school.programmers.co.kr/learn/courses/30/lessons/81301
 
 function solution(s) {
-  const object = {
-    0: 'zero',
-    1: 'one',
-    2: 'two',
-    3: 'three',
-    4: 'four',
-    5: 'five',
-    6: 'six',
-    7: 'seven',
-    8: 'eight',
-    9: 'nine',
-  };
+  const object = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
 
-  const length = Object.keys(object).length;
+  const length = object.length;
 
-  for (i = 0; i < length; i += 1) {
-    s = s.replace(object[i], i);
+  for (let i = 0; i < length; i += 1) {
+    //s = s.replace(object[i], i);
+    s = s.split(object[i]).join(i);
   }
   return Number(s);
 }

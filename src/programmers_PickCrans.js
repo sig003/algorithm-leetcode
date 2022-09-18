8448f6e174357837d015d7cb718fb4e4dc4d65a4
sig03 <sig03@naver.com>
2022-09-18 02:22:45 +0000

    add source

---
+++ src/programmers_PickCrans.js
@@ -0,0 +1,43 @@
+//https://school.programmers.co.kr/learn/courses/30/lessons/64061
+//board는 위에서부터 순서대로 쌓이는 것임
+function solution(board, moves) {
+  const array = [];
+  for (let i = 0; i < moves.length; i += 1) {
+    for (let j = 0; j < 5; j += 1) {
+      if (board[j][moves[i] - 1] !== 0) {
+        array.push(board[j][moves[i] - 1]);
+        board[j][moves[i] - 1] = 0;
+        break;
+      }
+    }
+  }
+
+  let count = 0;
+
+  while (true) {
+    let stop = true;
+    for (let i = 0; i < array.length; i += 1) {
+      if (array[i] === array[i + 1]) {
+        array.splice(i, 2);
+        count += 1;
+
+        stop = false;
+      }
+    }
+
+    if (stop === true) break;
+  }
+  return count * 2;
+}
+
+const board = [
+  [0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 3],
+  [0, 2, 5, 0, 1],
+  [4, 2, 4, 4, 2],
+  [3, 5, 1, 3, 1],
+];
+
+const moves = [1, 5, 3, 5, 1, 2, 1, 4];
+console.log(solution(board, moves));
+
